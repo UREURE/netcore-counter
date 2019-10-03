71e0b121095489f548c1a4b2afc5ca622cf2e145
Aurelio Pérez Noriega <aurelio.perez-ext@havasit.com>
2019-10-03 14:17:57 +0200

    Modifica start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 docker build -t counter .
 docker network create practica
-docker run -d --name redis-bd --network practica redis:5.0.3
+docker run -d --name redis-bd redis:5.0.3
 echo "http://localhost:5010/api/v1/swagger/index.html"
-docker run -it --rm -p 5010:5000 --network practica --name counter_5010 counter
+docker run -it --rm -p 5010:5000 --name counter_5010 counter
+docker container stop redis-bd
+docker container rm redis-bd
