f95c455145fc4af1345d66dc20668c49deb05821
Aurelio Pérez Noriega <aurelio.perez-ext@havasit.com>
2019-10-04 14:48:30 +0200

    Modifica despliegue en Kubernetes

--- k8s/start-kubernetes.sh
+++ k8s/start-kubernetes.sh
@@ -7,5 +7,5 @@ kubectl apply -f 02_redis-pvc.yaml --namespace=netcore-counter
 kubectl apply -f 03_redis-deployment.yaml --namespace=netcore-counter
 kubectl apply -f 04_redis-service.yaml --namespace=netcore-counter
 kubectl apply -f 05_counter-deployment.yaml --namespace=netcore-counter
-kubectl apply -f 06_counter-ingress.yaml --namespace=netcore-counter
-kubectl apply -f 07_counter-service.yaml --namespace=netcore-counter
+kubectl apply -f 06_counter-service.yaml --namespace=netcore-counter
+kubectl apply -f 07_counter-ingress.yaml --namespace=netcore-counter
