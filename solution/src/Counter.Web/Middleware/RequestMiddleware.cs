8f6c1153db54d7ad869eb58ff35d44d4f8816029
Aurelio Pérez Noriega <aurelio.perez-ext@havasit.com>
2019-10-08 17:46:50 +0200

    Añade métricas para Prometheus

---
+++ solution/src/Counter.Web/Middleware/RequestMiddleware.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Threading.Tasks;
+
+namespace Counter.Web.Middleware
+{
+    internal class RequestMiddleware
+    {
+        private readonly RequestDelegate next;
+
+        public RequestMiddleware(RequestDelegate next)
+        {
+            this.next = next;
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            string path = httpContext.Request.Path.Value;
+            string method = httpContext.Request.Method;
+
+            Prometheus.Counter counter = Prometheus.Metrics.CreateCounter(
+                "prometheus_request_total",
+                "HTTP Requests Total",
+                new Prometheus.CounterConfiguration { LabelNames = new[] { "path", "method", "status" } }
+                );
+
+            try
+            {
+                await next.Invoke(httpContext);
+            }
+            catch (Exception)
+            {
+                int statusCode = 500;
+                counter.Labels(path, method, statusCode.ToString()).Inc();
+
+                throw;
+            }
+
+            if (path != "/metrics")
+            {
+                counter.Labels(path, method, httpContext.Response.StatusCode.ToString()).Inc();
+            }
+        }
+    }
+}
--- solution/src/Counter.Web/Startup.cs
+++ solution/src/Counter.Web/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using NLog.Extensions.Logging;
+using Prometheus;
 using Swashbuckle.AspNetCore.Swagger;
 using System.IO;
 
@@ -97,6 +98,8 @@ namespace Counter.Web
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseMetricServer();
+            app.UseMiddleware<RequestMiddleware>();
             app.UseMiddleware<HttpExceptionMiddleware>();
             app.UseMvc();
 
