95caada74b32cee4428857408d04d634b10c33aa
UREURE <24934024+UREURE@users.noreply.github.com>
2019-10-09 15:16:44 +0200

    Añade exposición de métricas de Prometheus (#12)
    * Añade Web API
    
    * Modifica ruta Swagger
    
    * Mejora Dockerfile
    
    * Añade script para construir y arrancar la imagen
    
    * Corrige puertos docker run
    
    * Añade persistencia redis
    
    * Modifica start.sh
    
    * Corrige start.sh
    
    * Añade docker-compose.yml
    
    * Modifica Startup.cs
    
    * Modifica docker-compose.yml
    
    * Modifica docker-compose.yml
    
    * Añade configuración
    
    * Añade variables de entorno
    
    * Modifica variables de entorno
    
    * Añade variables de entorno
    
    * Añade variables de entorno
    
    * Corrige docker-compose.yml
    
    * Agrupa variables de configuración
    
    * Añade Test
    
    * Modifica Dockerfile
    
    * Añade declaraciones de Kubernetes
    
    * Añade documentación sobre Kubernetes
    
    * Corrige deployments de Kubernetes
    
    * Corrige 05_counter-deployment.yaml
    
    * Corrige 05_counter-deployment.yaml
    
    * Modifica despliegue en Kubernetes
    
    * Actualiza documentación
    
    * Corrige errores en documentación
    
    * Añade Chart de Helm
    
    * Utiliza Google.Cloud.Logging.NLog
    
    * Actualiza número de versión
    
    * Corrige versión en 01_counter-deployment.yaml
    
    * Revert 4e4c8f7c539d4cf6bc2e310c6b721ca8511d51d0
    
    * Revert "Utiliza Google.Cloud.Logging.NLog"
    
    * Añade direcciones a documentación
    
    * Corrige error de ortografía
    
    * Aumenta límites de Deployments e incorpora affinity y anti-affinity
    
    * Elimina affinity
    
    * Añade affinity y anti-affinity
    
    * Corrige 05_counter-deployment.yaml
    
    * Corrige error de publicación de logs no-JSON
    
    * Actualiza imagen de la aplicación a 0.3-unstable
    
    * Actualiza versión en variable de entorno
    
    * Corrige Affinity y Anti-affinity
    
    * Corrige Affinity y Anti-affinity
    
    * Añade métricas para Prometheus
    
    * Añade exposición de métricas de Prometheus

--- solution/build-and-push.sh
+++ solution/build-and-push.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+ARG1=${1:-0.3-unstable}
+
 docker build -t counter .
-docker tag counter ureure/netcore-counter:0.3-unstable
-docker push ureure/netcore-counter:0.3-unstable
+docker tag counter ureure/netcore-counter:$ARG1
+docker push ureure/netcore-counter:$ARG1
