81d0b2013a29eb2e11c9a93b75e0a99976619e3f
Aurelio Pérez Noriega <aurelio.perez-ext@havasit.com>
2019-10-24 10:45:26 +0200

    Establece versión de Helm

--- helm/helm_install.sh
+++ helm/helm_install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+VERSION=${1:-v2.14.3}
 
-./get_helm.sh
+./get_helm.sh --version $VERSION
 kubectl create -f ./tiller-rbac-config.yaml
-helm init --service-account tiller --history-max 200 --upgrade
+helm init --service-account tiller --history-max 200 --upgrade --wait
